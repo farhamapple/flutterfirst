d20108837eb60018adaeb31d5c2650ccaa206cdd
Farham <farhamapple@gmail.com>
2021-07-22 23:24:12 +0700

    First

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter First Apps',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Aplikasi Pertamaku'),
+        ),
+        body: Text('Hello World'),
+      ),
+    );
+  }
+}
