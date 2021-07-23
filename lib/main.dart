5093a8c0917526d99ea7b02cd3fc7b6f630d1fa4
Farham <farhamapple@gmail.com>
2021-07-23 11:02:01 +0700

    ListView

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,26 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<Widget> widgetList = [];
+  int counter = 1;
+  // Mengisi widget terlebih dahulu sebelum ditampilkan
+  // Pasang di constructor
+  // _MyAppState() {
+  //   for (int i = 1; i <= 20; i++) {
+  //     widgetList.add(Text(
+  //       'Data ke - ' + i.toString(),
+  //       style: TextStyle(fontSize: 50),
+  //     ));
+  //   }
+  // }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,27 +32,49 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.red,
       ),
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Aplikasi Pertamaku'),
-        ),
-        body: Center(
-          child: Container(
-            color: Colors.lightBlue,
-            width: 150,
-            height: 50,
-            child: Text(
-              'Saya adalah Farham Ganteng Banget testtestestsetsetestes',
-              maxLines: 2, // Wrap TextWidget
-              overflow: TextOverflow.ellipsis, // dikasih titik-titik
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  color: Colors.white,
-                  fontStyle: FontStyle.italic,
-                  fontWeight: FontWeight.bold),
-            ),
+          appBar: AppBar(
+            title: Text('Aplikasi ListView'),
           ),
-        ),
-      ),
+          body: ListView(
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(Colors.blue),
+                        textStyle:
+                            MaterialStateProperty.all(TextStyle(fontSize: 20))),
+                    onPressed: () {
+                      setState(() {
+                        widgetList.add(Text('Data ke - ' + counter.toString(),
+                            style: TextStyle(fontSize: 50)));
+                        counter++;
+                      });
+                    },
+                    child: const Text('Tambah Data'),
+                  ),
+                  ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(Colors.red),
+                        textStyle:
+                            MaterialStateProperty.all(TextStyle(fontSize: 20))),
+                    onPressed: () {
+                      setState(() {
+                        widgetList.removeLast();
+                        counter--;
+                      });
+                    },
+                    child: const Text('Hapus Data'),
+                  ),
+                ],
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: widgetList,
+              )
+            ],
+          )),
     );
   }
 }
