7976aca4b084dcb4eda140b66bceb04c939b18b9
Farham <farhamapple@gmail.com>
2021-07-23 10:24:54 +0700

    Fix: Widget Container New

--- lib/main.dart
+++ lib/main.dart
@@ -10,31 +10,31 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter First Apps',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Aplikasi Pertamaku'),
-        ),
-        body: Center(
-          child: Container(
-            color: Colors.lightBlue,
-            width: 150,
-            height: 50,
-            child: Text(
-              'Saya adalah Farham Ganteng Banget testtestestsetsetestes',
-              maxLines: 2, // Wrap TextWidget
-              overflow: TextOverflow.ellipsis, // dikasih titik-titik
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  color: Colors.white,
-                  fontStyle: FontStyle.italic,
-                  fontWeight: FontWeight.bold),
-            ),
+          appBar: AppBar(
+            title: Text('Aplikasi Pertamaku'),
           ),
-        ),
-      ),
+          body: Container(
+            color: Colors.lightBlue,
+            margin: EdgeInsets.all(20),
+            // margin: EdgeInsets.fromLTRB(20, 20, 20, 20), // Left Top Right Bottom
+            // margin: EdgeInsets.only(bottom: 20), // Only Bottom 20
+            padding: EdgeInsets.all(10),
+            child: Container(
+              //color: Colors.grey,
+              margin: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: <Color>[Colors.amber, Colors.grey])),
+            ),
+          )),
     );
   }
 }
